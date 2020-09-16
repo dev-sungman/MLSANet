6803788da7f79589f0da6fc0a47499977c0d4a59
dev.sungman@gmail.com <sungman_cho@nodee.mi2rl.co>
2020-09-16 15:45:49 +0900

    update gitignore

---
+++ models/acm_resnet.py
@@ -0,0 +1,258 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.acmblock import ACMBlock
+import time
+
+__all__ = ['ResNet', 'resnet18', 'resnet34', 'acm_resnet50', 'resnet101',
+           'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
+           'wide_resnet50_2', 'wide_resnet101_2']
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=512, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+
+        self.conv1 = nn.Conv2d(1, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        
+        self.acm1 = ACMBlock(in_channels=64*4)
+        self.acm2 = ACMBlock(in_channels=128*4)
+        self.acm3 = ACMBlock(in_channels=256*4)
+        self.acm4 = ACMBlock(in_channels=512*4)
+        
+        self.change_linear = nn.Linear(1024, 2)
+        self.disease_linear = nn.Linear(512, 2)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def apply_attention(self, x, k, q, cw):
+        k = k.view(k.shape[0], k.shape[1], 1, 1)
+        q = q.view(q.shape[0], q.shape[1], 1, 1)
+        att = x + k + q
+        out = att * cw
+        return att*cw
+    
+    def _forward_impl(self, x1, x2):
+        # See note [TorchScript super()]
+        x1 = self.conv1(x1)
+        x1 = self.bn1(x1)
+        x1 = self.relu(x1)
+        x1 = self.maxpool(x1)
+        
+        x2 = self.conv1(x2)
+        x2 = self.bn1(x2)
+        x2 = self.relu(x2)
+        x2 = self.maxpool(x2)
+        
+        x1 = self.layer1(x1)
+        x2 = self.layer1(x2)
+        x1, x2, orth_loss1 = self.acm1(x1, x2)
+        
+        x1 = self.layer2(x1)
+        x2 = self.layer2(x2)
+        x1, x2, orth_loss2 = self.acm2(x1, x2)
+        
+        x1 = self.layer3(x1)
+        x2 = self.layer3(x2)
+        x1, x2, orth_loss3 = self.acm3(x1, x2)
+        
+        x1 = self.layer4(x1)
+        x2 = self.layer4(x2)
+        x1, x2, orth_loss4 = self.acm4(x1, x2)
+        
+        x1 = self.avgpool(x1)
+        x2 = self.avgpool(x2)
+
+        x1 = torch.flatten(x1, 1)
+        x2 = torch.flatten(x2, 1)
+
+        x1 = self.fc(x1)
+        x2 = self.fc(x2)
+        
+        cat = torch.cat((x1, x2), 1)
+        out = self.change_linear(cat)
+        
+        orth_score = (orth_loss1 + orth_loss2 + orth_loss3 + orth_loss4) / 4
+        
+        x1 = self.disease_linear(x1)
+        x2 = self.disease_linear(x2)
+
+        return x1, x2, out, orth_score
+
+    def forward(self, x1, x2):
+        return self._forward_impl(x1, x2)
+
+def _resnet(arch, block, layers, pretrained, progress, **kwargs):
+    model = ResNet(block, layers, **kwargs)
+    return model
+
+def acm_resnet50(pretrained=False, progress=True, **kwargs):
+    r"""ResNet-50 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress,
+                   **kwargs)
---
+++ models/acmblock.py
@@ -0,0 +1,117 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ACMBlock(nn.Module):
+    def __init__(self, in_channels):
+        super(ACMBlock, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = in_channels
+        self.k_conv = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.out_channels, (1,1)),
+        )
+
+        self.q_conv = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.out_channels, (1,1)),
+        )
+
+        self.global_pooling = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.out_channels//2, (1,1)),
+            nn.ReLU(),
+            nn.Conv2d(self.out_channels//2, self.out_channels, (1,1)),
+            nn.Sigmoid()
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.normalize = nn.Softmax(dim=3)
+
+    def _get_normalized_features(self, x):
+        ''' Get mean vector by channel axis '''
+        
+        c_mean = self.avgpool(x)
+        return c_mean
+
+    def get_K(self, x):
+        K = self.k_conv(x)
+
+        b, c, h, w = K.shape
+
+        K = K.view(b, c, 1, h*w)
+        K = self.normalize(K)
+        K = K.view(b, c, h, w)
+
+        K = torch.einsum('nchw,nchw->nc',[K, x])
+
+        return K
+
+    def get_Q(self, x):
+        Q = self.q_conv(x)
+
+        b, c, h, w = Q.shape
+
+        Q = Q.view(b, c, 1, h*w)
+        Q = self.normalize(Q)
+        Q = Q.view(b, c, h, w)
+
+        Q = torch.einsum('nchw,nchw->nc',[Q, x])
+
+        return Q
+    
+    def get_channel_weights(self, x):
+        mean_features = self._get_normalized_features(x)
+        normalized_features = x - mean_features
+        
+        channel_weights = self.global_pooling(mean_features)
+
+        return channel_weights
+    
+    def get_orth_loss(self, k, q):
+        cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        orth_loss = cos(k, q)
+        orth_loss = torch.mean(orth_loss, dim=0)
+        #orth_loss = torch.mean(k*q, dim=1, keepdim=True)
+        return orth_loss
+
+    def forward(self, x1, x2):
+        K = self.k_conv(x1)
+        Q = self.q_conv(x2)
+
+        b, c, h, w = K.shape
+
+        K = K.view(b, c, 1, h*w)
+        K = self.normalize(K)
+        K = K.view(b, c, h, w)
+
+        Q = Q.view(b, c, 1, h*w)
+        Q = self.normalize(Q)
+        Q = Q.view(b, c, h, w)
+
+        K = torch.einsum('nchw,nchw->nc',[K, x1])
+        Q = torch.einsum('nchw,nchw->nc',[Q, x2])
+        K = K.view(K.shape[0], K.shape[1], 1, 1)
+        Q = Q.view(Q.shape[0], Q.shape[1], 1, 1)
+
+        mean_features1 = self._get_normalized_features(x1)
+        mean_features2 = self._get_normalized_features(x2)
+        channel_weights1 = self.global_pooling(mean_features1)
+        channel_weights2 = self.global_pooling(mean_features2)
+        
+        out1 = x1 + K + Q
+        out2 = x2 + K + Q
+        
+        #out1 = x1 + (K - Q)
+        #out2 = x2 + (K - Q)
+        out1 = channel_weights1 * out1
+        out2 = channel_weights2 * out2
+        
+        orth_loss = self.get_orth_loss(K,Q)
+
+        return out1, out2, orth_loss
+
+### Test
+if __name__ == '__main__':
+    test_vector = torch.randn((1,2,3,3), dtype=torch.float32)
+    acm = ACMBlock(in_channels=test_vector.shape[1])
+
+    result = acm(test_vector)
