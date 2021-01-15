d509a2c525ebbae9ac55d25c30cdcb5134906b53
Sungman-Cho <pccho9108@gmail.com>
2021-01-15 17:04:39 +0900

    fixed typo error

--- utils.py
+++ utils.py
@@ -25,8 +25,8 @@ def register_forward_hook(model):
     return activation, layer_names
 
 def visualize_activation_map(activation, layer_names, iter_, phase, img_dir, preds, labels, base, fu):
-    img_mean = 0.4
-    img_std = 0.2
+    img_mean = 0.2
+    img_std = 0.4
     label_names = ['change', 'nochange']
     acts = []
     num_layers = len(layer_names)
@@ -55,7 +55,7 @@ def visualize_activation_map(activation, layer_names, iter_, phase, img_dir, pre
             np_fu = fu[batch,0,:,:].cpu().detach().numpy()
             np_fu = cv2.resize(np_fu, (512, 512))
             np_fu = cv2.cvtColor(np_fu, cv2.COLOR_GRAY2BGR)
-            np_fu = (np_fu*img_mean) + img_std
+            np_fu = (np_fu*img_std) + img_mean
 
             np_base_act = acts[0][batch,:,:].cpu().detach().numpy()
             np_fu_act = acts[1][batch,:,:].cpu().detach().numpy()
